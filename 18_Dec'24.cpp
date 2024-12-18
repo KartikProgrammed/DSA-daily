0ecdfe90b0f1ea8c993e02ec333b61bff2d3179a
Kartik <gkhatri211@gmail.com>
2024-12-18 16:15:56 +0530

    DailyProblem

--- 18_Dec'24.cpp
+++ 18_Dec'24.cpp
@@ -69,3 +69,32 @@ public:
         return result;
     }
 };
+
+
+//1475. Final Prices With a Special Discount in a Shop
+// You are given an integer array prices where prices[i] is the price of the ith item in a shop.
+// There is a special discount for items in the shop. If you buy the ith item, then you will receive a discount equivalent to prices[j] where j is the minimum index such that j > i and prices[j] <= prices[i]. Otherwise, you will not receive any discount at all.
+// Return an integer array answer where answer[i] is the final price you will pay for the ith item of the shop, considering the special discount.
+
+//CODE:-
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        stack<int> helper;
+        vector<int> result(prices.size(),0);
+        for(int i=0;i<prices.size();i++){
+            while(!helper.empty() && prices[helper.top()]>=prices[i]){
+                int curr=helper.top();
+                result[curr]=prices[curr]-prices[i];
+                helper.pop();
+            }
+            helper.push(i);
+        }
+        while(!helper.empty()) {
+            int idx = helper.top();
+            result[idx] = prices[idx];
+            helper.pop();
+        }
+        return result;
+    }
+};
