1a4a6254154da25cf3a368aea25f3514d95ca607
Kartik <gkhatri211@gmail.com>
2024-12-19 17:04:32 +0530

    MaxChunks

--- 19_Dec'24.cpp
+++ 19_Dec'24.cpp
@@ -79,3 +79,28 @@ public:
     }
 
 };
+
+//769. Max Chunks To Make Sorted
+// You are given an integer array arr of length n that represents a permutation of the integers in the range [0, n - 1].
+
+// We split arr into some number of chunks (i.e., partitions), and individually sort each chunk. After concatenating them, the result should equal the sorted array.
+
+// Return the largest number of chunks we can make to sort the array.
+
+//CODE:-
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int maxVal = 0; 
+        int count = 0;  
+
+        for (int i = 0; i < arr.size(); i++) {
+            maxVal = max(maxVal, arr[i]); 
+            if (maxVal == i) { 
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
