7de66f96028561b6476dd36abc23923799fd9d32
Kartik <gkhatri211@gmail.com>
2024-12-20 22:41:09 +0530

    DailyProblem

--- 20_Dec'24.cpp
+++ 20_Dec'24.cpp
@@ -16,17 +16,17 @@ using namespace std;
 // The level of a node is the number of edges along the path between it and the root node.
 
 //CODE:-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+// Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
 class Solution {
 public:
     void helper(TreeNode* leftN,TreeNode* rightN,int currdepth){
