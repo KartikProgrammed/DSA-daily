f07562868585f66f7d8c129eecb6cba4c1611c89
KartikProgrammed <113375209+KartikProgrammed@users.noreply.github.com>
2024-12-23 23:54:21 +0530

    SumOfSubarrayMinimums

--- 23_Dec'24.cpp
+++ 23_Dec'24.cpp
@@ -52,3 +52,49 @@ public:
         return swaps;
     }
 };
+
+//907. Sum of Subarray Minimums
+// Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 109 + 7.
+
+//approach:-
+//finding minimum element after and before each element and calculating the total as required (take mod 1e9+7 as said by the question at the end)
+//CODE:
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    int sumSubarrayMins(vector<int>& arr) {
+        stack<int> helper;
+        int n=arr.size();
+        int tot=0;
+        vector<int> nextSmaller(n,n);
+        vector<int> prevSmaller(n,-1);
+        for(int i=0;i<arr.size();i++){
+            while(!helper.empty() && arr[helper.top()]>arr[i]){
+                helper.pop();
+            }
+            if(!helper.empty()){
+                prevSmaller[i]=helper.top();
+            }
+            helper.push(i);
+        }
+        while(!helper.empty()){
+            helper.pop();
+        }
+        for(int i=n-1;i>=0;i--){
+            while(!helper.empty() && arr[helper.top()]>=arr[i]){
+                helper.pop();
+            }
+            if(!helper.empty()){
+                nextSmaller[i]=helper.top();
+            }
+            helper.push(i);
+        }
+        for(int i=0;i<n;i++){
+            long long left =i -  prevSmaller[i];
+            long long right = nextSmaller[i] - i;
+            tot = (tot + arr[i] * left * right) % MOD;
+        }
+        return tot;
+    }
+};
