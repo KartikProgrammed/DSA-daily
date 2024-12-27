b25e07b9b08c72d47acfe641073d03a22a124da7
KartikProgrammed <113375209+KartikProgrammed@users.noreply.github.com>
2024-12-27 17:41:36 +0530

    SumOfSubarrayRange

--- 27_Dec'24.cpp
+++ 27_Dec'24.cpp
@@ -48,3 +48,88 @@ public:
         return maxtillhere;
     }
 };
+
+//2104. Sum of Subarray Ranges
+// You are given an integer array nums. The range of a subarray of nums is the difference between the largest and smallest element in the subarray.
+
+// Return the sum of all subarray ranges of nums.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+//APPROACH:-
+//finding subarray miniums and subarray maximums and then subtracting easy
+//sub-min and sub-max are found using monotonic stack
+//NSL- next smaller left, NSR- next smaller right similarly NGL and NGR
+
+//CODE:-
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        stack<int> helper;
+        vector<int> NSL(nums.size(),-1);
+        vector<int> NSR(nums.size(),nums.size());
+        for(int i=0;i<nums.size();i++){
+            while(!helper.empty() && nums[helper.top()]>=nums[i]){
+                helper.pop();
+            }
+            if(!helper.empty()){
+                NSL[i]=helper.top();
+            }
+            helper.push(i);
+        }
+        while(!helper.empty()){
+            helper.pop();
+        }
+        for(int i=nums.size()-1;i>=0;i--){
+            while(!helper.empty() && nums[helper.top()]>nums[i]){
+                helper.pop();
+            }
+            if(!helper.empty()){
+                NSR[i]=helper.top();
+            }
+            helper.push(i);
+        }
+        //calc contribution
+        long long rangemin=0;
+        for(int i=0;i<nums.size();i++){
+            long long left=i-NSL[i];
+            long long right=NSR[i]-i;
+            rangemin=rangemin+((long long)nums[i]*left*right);
+        }
+        while(!helper.empty()){
+            helper.pop();
+        }
+        vector<int> NGR(nums.size(),nums.size());
+        vector<int> NGL(nums.size(),-1);
+        for(int i=0;i<nums.size();i++){
+            while(!helper.empty() && nums[helper.top()]<=nums[i]){
+                helper.pop();
+            }
+            if(!helper.empty()){
+                NGL[i]=helper.top();
+            }
+            helper.push(i);
+        }
+        while(!helper.empty()){
+            helper.pop();
+        }
+        for(int i=nums.size()-1;i>=0;i--){
+            while(!helper.empty() && nums[helper.top()]<nums[i]){
+                helper.pop();
+            }
+            if(!helper.empty()){
+                NGR[i]=helper.top();
+            }
+            helper.push(i);
+        }
+        //calc contribution
+        long long rangemax=0;
+        for(int i=0;i<nums.size();i++){
+            long long left=i-NGL[i];
+            long long right=NGR[i]-i;
+            rangemax=rangemax+((long long)nums[i]*left*right);
+        }
+        return rangemax-rangemin;
+    }
+};
