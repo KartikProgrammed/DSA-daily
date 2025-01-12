00c4351bcfb9d5ec7237d479c03ac18adde3a14c
Kartik <gkhatri211@gmail.com>
2025-01-12 14:24:28 +0530

    CheckIfAParenthesisStringCanBeValid

---
+++ 12_Jan'25.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include<string>
+#include<unordered_map>
+#include<stack>
+#include<unordered_set>
+using namespace std;
+
+//2116. Check if a Parentheses String Can Be Valid
+// A parentheses string is a non-empty string consisting only of '(' and ')'. It is valid if any of the following conditions is true:
+
+// It is ().
+// It can be written as AB (A concatenated with B), where A and B are valid parentheses strings.
+// It can be written as (A), where A is a valid parentheses string.
+// You are given a parentheses string s and a string locked, both of length n. locked is a binary string consisting only of '0's and '1's. For each index i of locked,
+
+// If locked[i] is '1', you cannot change s[i].
+// But if locked[i] is '0', you can change s[i] to either '(' or ')'.
+// Return true if you can make s a valid parentheses string. Otherwise, return false.
+
+//APPROACH:-
+//maintain a var open which counts the number of opening brackets + flexible brackets
+//reduce the var open for closing brackets which are not flexible
+//if the no of flexible brackets/( are less than ) return false
+//do it from the backwards too to ensure correct output
+
+
+//CODE:-
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        if(s.length()%2!=0){
+            return false;
+        }
+        int open=0;
+        for(int i=0;i<s.length();i++){
+            if(locked[i]=='0' || s[i]=='('){
+                open++;
+            }
+            else{
+                open--;
+            }
+            if(open<0)
+                return false;
+        }
+
+        open=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(locked[i]=='0' || s[i]==')'){
+                open++;
+            }
+            else{
+                open--;
+            }
+            if(open<0){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+};
