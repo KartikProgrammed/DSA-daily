396b4541afa625d3f8f61e7bc5cbda9659bd0ca2
Kartik <gkhatri211@gmail.com>
2025-01-27 23:25:05 +0530

    MergeKSinglyLinkedList

--- 27_Jan'25.cpp
+++ 27_Jan'25.cpp
@@ -97,16 +97,16 @@ public:
 // send all the node val to a min heap priority queue and then create a linkedlist out of it
 
 //CODE:-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+//  * Definition for singly-linked list.
+struct ListNode {
+     int val;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode *next;
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
