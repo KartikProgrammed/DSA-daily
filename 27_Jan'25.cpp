9818534a0227ef241f672f127fbd231e697ff58c
Kartik <gkhatri211@gmail.com>
2025-01-27 23:49:39 +0530

    TaskScheduler

--- 27_Jan'25.cpp
+++ 27_Jan'25.cpp
@@ -128,3 +128,32 @@ public:
         return head->next;
     }
 };
+
+// 621. Task Scheduler
+// You are given an array of CPU tasks, each labeled with a letter from A to Z, and a number n. Each CPU interval can be idle or allow the completion of one task. Tasks can be completed in any order, but there's a constraint: there has to be a gap of at least n intervals between two tasks with the same label.
+
+// Return the minimum number of CPU intervals required to complete all tasks.
+
+//APPRAOCH:-
+// use a formula int intervals = (maxFreq - 1) * (n + 1) + maxFreqCount;
+
+//CODE:-
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> map(26,0);
+        for(int i=0;i<tasks.size();i++){
+            int curr=tasks[i]-'A';
+            map[curr]++;
+        }
+        int maxi=*max_element(map.begin(),map.end());
+        int max_eleFreq=0;
+        for(int i=0;i<26;i++){
+            if(map[i]==maxi){
+                max_eleFreq++;
+            }
+        }
+        int intervals = (maxi - 1) * (n + 1) + max_eleFreq;
+        return max(intervals, (int)tasks.size());
+    }
+};
