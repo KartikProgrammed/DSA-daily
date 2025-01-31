35a71c92f7e66f689a6877e8d52deb7a6421e6c6
Kartik <gkhatri211@gmail.com>
2025-01-31 14:31:28 +0530

    KthLargestElementInAStream

--- 31_Jan'25.cpp
+++ 31_Jan'25.cpp
@@ -75,3 +75,54 @@ public:
         return res;
     }
 };
+
+
+// 703. Kth Largest Element in a Stream
+// You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+
+// You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+
+// Implement the KthLargest class:
+
+// KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+// int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+ 
+
+//APPROACH:-
+// use a min heap to find the kth largest element 
+//by default pop elements when the size goes beyond k and in the add function manipulate the input and push accordingly
+
+//CODE:-
+class KthLargest {
+private:
+    int elements;
+    priority_queue<int,vector<int>,greater<int>> pq;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        elements=k;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if (pq.size() > elements) {
+                pq.pop(); // Maintain only k largest elements in the heap
+            }
+        }
+        
+    }
+    
+    int add(int val) {
+        if(pq.size()<elements){
+            pq.push(val);
+        }
+        else if(val>pq.top()){
+            pq.pop();
+            pq.push(val);
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
