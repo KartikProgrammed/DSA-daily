b106405ff85c334a72d4a41914f9954491e1dbae
Kartik <gkhatri211@gmail.com>
2025-02-08 21:35:04 +0530

    JumpGameII

--- 8_Feb'25.cpp
+++ 8_Feb'25.cpp
@@ -119,3 +119,38 @@ class Solution {
             return true;
         }
     };
+
+
+//45. Jump Game II
+// You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+// Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+// 0 <= j <= nums[i] and
+// i + j < n
+// Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+
+//APPROACH:-
+//create a current end var and a maxreach var to calculate the max reachable point 
+//break when the current end matches last index
+
+//CODE:- (Beats 100%)
+class Solution {
+    public:
+        int jump(vector<int>& nums) {
+            int count=0;
+            int maxreach=0;
+            int currend=0;
+            for(int i=0;i<nums.size()-1;i++){
+                maxreach = max(maxreach, i + nums[i]);
+                if(i==currend){
+                    count++;
+                    currend = maxreach;
+                    if (currend >= nums.size() - 1)
+                        break;
+                }
+            }
+            return count;
+        }
+    };
