fdea49b71ca35b0942c6c625ae6d12e1e7743f04
Kartik <gkhatri211@gmail.com>
2025-02-10 19:32:18 +0530

    Candy

--- 10_Feb'25.cpp
+++ 10_Feb'25.cpp
@@ -46,3 +46,41 @@ class Solution {
             return s;
         }
     };
+
+
+//135. Candy
+// There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+
+// You are giving candies to these children subjected to the following requirements:
+
+// Each child must have at least one candy.
+// Children with a higher rating get more candies than their neighbors.
+// Return the minimum number of candies you need to have to distribute the candies to the children.
+
+//APPROACH:-
+//1st iteration from left to right increase the candies if LHS > curr
+//2nd iteration from right to left increase the candies if RHS>curr && new arr curr<RHS
+//find total sum
+
+//CODE:- (beats 100%)
+class Solution {
+    public:
+        int candy(vector<int>& ratings) {
+            vector<int> arr(ratings.size(),1);
+            for(int i=1;i<ratings.size();i++){
+                if(ratings[i]>ratings[i-1]){
+                    arr[i]=arr[i-1]+1;
+                }
+            }
+            for(int i=ratings.size()-2;i>=0;i--){
+                if(ratings[i]>ratings[i+1] && arr[i]<=arr[i+1]){
+                    arr[i]=arr[i+1]+1;
+                }
+            }
+            int tot=0;
+            for(int i=0;i<ratings.size();i++){
+                tot+=arr[i];
+            }
+            return tot;
+        }
+    };
