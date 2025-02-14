b631584f2d1c8f1e966517faa080d52c58370c51
Kartik <gkhatri211@gmail.com>
2025-02-14 12:41:05 +0530

    Non-OverlappingIntervals

--- 14_Feb'25.cpp
+++ 14_Feb'25.cpp
@@ -117,3 +117,40 @@ class Solution {
             return res;
         }
     };
+
+
+//435. Non-overlapping Intervals
+// Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+// Note that intervals which only touch at a point are non-overlapping. For example, [1, 2] and [2, 3] are non-overlapping.
+
+//APPROACH:-
+//compute prev pointer which points at prev intervals end and curr intervals start
+//update prev pointer accordingly such that it we remove the larger interval so as to accomadate max intervals
+//increment count 
+
+//CODE:-
+class Solution {
+    public:
+        int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+            sort(intervals.begin(),intervals.end());
+            int count=0;
+            int n=intervals.size();
+            if(n<=1){
+                return 0;
+            }
+            int prev=intervals[0][1];
+            int i=1;
+            while(i<n){
+                if(prev>intervals[i][0]){
+                    count++;
+                    prev=min(prev,intervals[i][1]);
+                }
+                else{
+                    prev=intervals[i][1];
+                }
+                i++;
+            }
+            return count;
+        }
+    };
