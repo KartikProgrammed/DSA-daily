d859ccb3a212e158a6ae76fcc7f08c1412a5e266
Kartik <gkhatri211@gmail.com>
2025-03-12 22:33:12 +0530

    MaxDepthBinTree

--- 12_March'25.cpp
+++ 12_March'25.cpp
@@ -35,4 +35,40 @@ class Solution {
             pos=nums.size()-pos;
             return max(neg,pos);
         }
+};
+
+//104. Maximum Depth of Binary Tree
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+
+//CODE:-
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+    public:
+        void recursion(int depth,int &maxdepth,TreeNode* root){
+            if(root==NULL){
+                return ;
+            }
+            maxdepth=max(depth,maxdepth);
+            recursion(depth+1,maxdepth,root->right);
+            recursion(depth+1,maxdepth,root->left);
+        }
+        int maxDepth(TreeNode* root) {
+            if(root==NULL){
+                return 0;
+            }
+            int maxdepth=0;
+            int depth=1;
+            recursion(depth,maxdepth,root);
+            return maxdepth;
+        }
     };
