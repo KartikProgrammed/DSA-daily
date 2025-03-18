782b3e2399c599bd5cb2cc8de5f8b653773d0756
Kartik <gkhatri211@gmail.com>
2025-03-18 20:37:42 +0530

    SymmetryTree

--- 18_March'25.cpp
+++ 18_March'25.cpp
@@ -185,3 +185,42 @@ public:
         return res;
     }
 };
+
+
+
+//101. Symmetric Tree
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+//APPROACH
+//RECURSION
+
+//CODE:-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool recursion(TreeNode* t1,TreeNode *t2){
+        if(!t1&&!t2)
+            return true;
+        if(!t1||!t2){
+            return false;
+        }
+        bool op1=recursion(t1->left,t2->right);
+        bool op2=recursion(t1->right,t2->left);
+        return (t1->val==t2->val)&&op1&&op2;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root==NULL)
+            return true;
+        return recursion(root->left,root->right);
+    }
+};
