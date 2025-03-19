d1bbf7a6b4788bc07a562862f0b11739b3b3f2d6
Kartik <gkhatri211@gmail.com>
2025-03-19 20:31:07 +0530

    NoofNodesIn<nCompl

--- 19_March'25.cpp
+++ 19_March'25.cpp
@@ -226,3 +226,59 @@ public:
     }
 };
 
+
+
+// 222. Count Complete Tree Nodes
+// Given the root of a complete binary tree, return the number of the nodes in the tree.
+// According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+// Design an algorithm that runs in less than O(n) time complexity.
+
+
+//APPROACH:-
+//TC(LOGN^2)
+//compute heights of left and right subtr 
+//if its same we have complete bt otherwise split the tree into 2 trees
+//now recursively find the height in this way
+
+//CODE:-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int righth(TreeNode* root){
+        int height=1;
+        while(root->right){
+            root=root->right;
+            height++;
+        }
+        return height;
+    }
+    int lefth(TreeNode* root){
+        int height=1;
+        while(root->left){
+            root=root->left;
+            height++;
+        }
+        return height;
+    }
+    int countNodes(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int rh=righth(root);
+        int lh=lefth(root);
+        if(lh==rh){
+            return(1<<lh)-1;
+        }
+        return countNodes(root->left)+countNodes(root->right)+1;
+    }
+};
