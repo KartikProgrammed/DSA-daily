7f03e9771d503a2bbe46dbfa8f40fa4b31972aa4
Kartik <gkhatri211@gmail.com>
2025-03-21 17:21:16 +0530

    inorderMorris

--- 21_March'25.cpp
+++ 21_March'25.cpp
@@ -133,3 +133,52 @@ public:
         prev=root;
     }
 };
+
+
+//94. Binary Tree Inorder Traversal
+//Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+//APPROACH:-
+//MORRIS-CREATE THREADS
+
+//CODE:-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        TreeNode* curr=root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* prev=curr->left;
+                while(prev->right!=NULL && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return inorder;
+    }
+};
