b5201be6259f61b90a40632113c7bc21c47bd15c
Kartik <gkhatri211@gmail.com>
2025-03-23 20:13:41 +0530

    insertBST

--- 23_March'25.cpp
+++ 23_March'25.cpp
@@ -95,3 +95,54 @@ public:
         
     }
 };
+
+
+//701. Insert into a Binary Search Tree
+//You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+// Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+//APPROACH:-
+//BASICS OF BST
+
+//CODE:-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void recursion(TreeNode* root,int val){
+        if(root->val<val){
+            if(root->right==NULL){
+                TreeNode* temp=new TreeNode(val);
+                root->right=temp;
+                return;
+            }
+            recursion(root->right,val);
+        }
+        else if(root->val>val){
+            if(root->left==NULL){
+                TreeNode* temp=new TreeNode(val);
+                root->left=temp;
+                return;
+            }
+            recursion(root->left,val);
+        }
+    }
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+            TreeNode* temp=new TreeNode(val);
+            return temp;
+        }
+        TreeNode* temp=root;
+        recursion(root,val);
+        return temp;
+    }
+};
