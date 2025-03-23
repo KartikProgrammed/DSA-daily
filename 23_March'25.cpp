3268c3cf4eede5e05316885aa94b117847ba138a
Kartik <gkhatri211@gmail.com>
2025-03-23 22:33:05 +0530

    kthLargestElement

--- 23_March'25.cpp
+++ 23_March'25.cpp
@@ -249,3 +249,47 @@ public:
         return root;
     }
 };
+
+
+//230. Kth Smallest Element in a BST
+//Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+//APPROACH:-
+//priority queue
+
+//CODE:-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void input(TreeNode* root,priority_queue<int,vector<int>,greater<int>>& pq){
+        if(root==NULL){
+            return;
+        }
+        pq.push(root->val);
+        if(root->left){
+            input(root->left,pq);
+        }
+        if(root->right){
+            input(root->right,pq);
+        }
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+        input(root,pq);
+        while(!pq.empty()&&k>1){
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
