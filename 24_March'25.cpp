b53d9014eaaefcf1cf617d2b6dafd3c52c9db706
Kartik <gkhatri211@gmail.com>
2025-03-24 20:52:18 +0530

    preorder->BST

--- 24_March'25.cpp
+++ 24_March'25.cpp
@@ -150,3 +150,62 @@ public:
         return root;
     }
 };
+
+
+//1008. Construct Binary Search Tree from Preorder Traversal
+//Given an array of integers preorder, which represents the preorder traversal of a BST (i.e., binary search tree), construct the tree and return its root.
+// It is guaranteed that there is always possible to find a binary search tree with the given requirements for the given test cases.
+// A binary search tree is a binary tree where for every node, any descendant of Node.left has a value strictly less than Node.val, and any descendant of Node.right has a value strictly greater than Node.val.
+// A preorder traversal of a binary tree displays the value of the node first, then traverses Node.left, then traverses Node.right.
+
+
+//APPROACH:-
+//the root is the 1st element of the tree
+//insert all other elements according to bst rules
+
+//CODE:-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void insertBST(TreeNode* root,int val){
+        TreeNode* ins=new TreeNode(val);
+        TreeNode* temp=root;
+        TreeNode* prev=temp;
+        while(temp!=NULL){
+            if(temp->val < val){
+                prev=temp;
+                temp=temp->right;
+            }
+            else{
+                prev=temp;
+                temp=temp->left;
+            }
+        }
+        if(prev->val < val){
+            prev->right=ins;
+        }
+        else{
+            prev->left=ins;
+        }
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        if(preorder.size()==0){
+            return NULL;
+        }
+        TreeNode* root=new TreeNode(preorder[0]);
+        for(int i=1;i<preorder.size();i++){
+            insertBST(root,preorder[i]);
+        }
+        return root;
+    }
+};
