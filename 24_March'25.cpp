e3df37e342cf17fcff7e77408e879d103ed06776
Kartik <gkhatri211@gmail.com>
2025-03-24 15:02:41 +0530

    validBST

--- 24_March'25.cpp
+++ 24_March'25.cpp
@@ -74,3 +74,47 @@ public:
         return days-res;
     }
 };
+
+
+//98. Validate Binary Search Tree
+// Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+// A valid BST is defined as follows:
+// The left subtree of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+
+//APPROACH:-
+//BST'S inorder is always sorted
+
+//CODE:-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inordertrav(TreeNode* root,vector<int> &inorder){
+        if(root!=NULL){
+            inordertrav(root->left,inorder);
+            inorder.push_back(root->val);
+            inordertrav(root->right,inorder);
+        }
+    }
+    bool isValidBST(TreeNode* root) {
+        vector<int> inorder;
+        inordertrav(root,inorder);
+        for(int i=1;i<inorder.size();i++){
+            if(inorder[i]<=inorder[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
