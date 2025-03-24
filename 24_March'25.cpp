d095d283f0e550d50d062b5e8890ea2d19c59999
Kartik <gkhatri211@gmail.com>
2025-03-24 15:02:58 +0530

    validBST

--- 24_March'25.cpp
+++ 24_March'25.cpp
@@ -87,17 +87,14 @@ public:
 //BST'S inorder is always sorted
 
 //CODE:-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution {
 public:
     void inordertrav(TreeNode* root,vector<int> &inorder){
