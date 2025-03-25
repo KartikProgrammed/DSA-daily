b4b18015adadbb2022fdd826246f47d9c485caaf
Kartik <gkhatri211@gmail.com>
2025-03-25 22:36:20 +0530

    recoverBST

--- 25_March'25.cpp
+++ 25_March'25.cpp
@@ -167,3 +167,39 @@ public:
         return false;
     }
 };
+
+
+//99. Recover Binary Search Tree
+// You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+//CODE:-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *first = nullptr, *second = nullptr, *prev = nullptr;
+    void inorder(TreeNode* root) {
+        if (!root) return;
+        inorder(root->left);
+        if (prev && prev->val > root->val) {
+            if (!first) first = prev; 
+            second = root;
+        }
+        prev = root; 
+        inorder(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        if (first && second) swap(first->val, second->val);
+    }
+};
