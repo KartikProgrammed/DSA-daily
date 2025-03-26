8c8faf9fa44ff2312101b7c2b1e9618457096847
Kartik <gkhatri211@gmail.com>
2025-03-26 16:19:04 +0530

    No.OfProvinces

--- 26_March'25.cpp
+++ 26_March'25.cpp
@@ -148,3 +148,53 @@ class Solution {
     return res;
     }
 };
+
+
+//547. Number of Provinces
+// There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+// A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+// You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+// Return the total number of provinces.
+
+
+//APPROACH:-
+//BFS
+
+//CODE:-
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+       int v=isConnected.size();
+       int res=0;
+        if(v==0){
+            return res;
+        }
+        vector<int> visited(v,false);
+        queue<int> q;
+        vector<vector<int>> adj(v);
+        for(int i=0;i<v;i++){
+            for(int j=0;j<v;j++){
+                if(isConnected[i][j]==1)
+                    adj[i].push_back(j);
+            }
+        } 
+        for(int st=0;st<v;st++){
+            if(!visited[st]){
+                res++;
+                q.push(st);
+                visited[st]=true;
+                while(!q.empty()){
+                    int curr=q.front();
+                    q.pop();
+                    for(int neigh=0;neigh<adj[curr].size();neigh++){
+                        if(!visited[adj[curr][neigh]]){
+                            q.push(adj[curr][neigh]);
+                            visited[adj[curr][neigh]]=true;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
