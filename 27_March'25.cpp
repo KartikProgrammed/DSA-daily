d57df9776a1f9e555b908f939c43fb014143eb07
Kartik <gkhatri211@gmail.com>
2025-03-28 00:21:28 +0530

    SurroundedRegions

--- 27_March'25.cpp
+++ 27_March'25.cpp
@@ -146,3 +146,67 @@ public:
         return res;
     }
 };
+
+
+//130. Surrounded Regions
+// You are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+// Connect: A cell is connected to adjacent cells horizontally or vertically.
+// Region: To form a region connect every 'O' cell.
+// Surround: The region is surrounded with 'X' cells if you can connect the region with 'X' cells and none of the region cells are on the edge of the board.
+// To capture a surrounded region, replace all 'O's with 'X's in-place within the original board. You do not need to return anything.
+
+//APPROACH:-
+//DFS - poorly worded - eliminate all Os who are not connected to a O on the edge
+
+//CODE:-
+//1st try
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board,vector<vector<bool>>& keep,int x,int y){
+        if(!keep[x][y]){
+            int m=board.size();
+            int n=board[0].size();
+            keep[x][y]=true;
+            if(x>0 && board[x-1][y]=='O'){
+                dfs(board,keep,x-1,y);
+            }
+            if(x<m-1 && board[x+1][y]=='O'){
+                dfs(board,keep,x+1,y);
+            }
+            if(y>0 && board[x][y-1]=='O'){
+                dfs(board,keep,x,y-1);
+            }
+            if(y<n-1 && board[x][y+1]=='O'){
+                dfs(board,keep,x,y+1);
+            }
+        }
+    }
+    void solve(vector<vector<char>>& board) {
+        int m=board.size();
+        int n=board[0].size();
+        vector<vector<bool>> keep(m,vector<bool>(n,false));
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O'){
+                dfs(board,keep,i,0);
+            }
+            if(board[i][n-1]=='O'){
+                dfs(board,keep,i,n-1);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(board[0][i]=='O'){
+                dfs(board,keep,0,i);
+            }
+            if(board[m-1][i]=='O'){
+                dfs(board,keep,m-1,i);
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!keep[i][j] && board[i][j]=='O'){
+                    board[i][j]='X';
+                }
+            }
+        }
+    }
+};
