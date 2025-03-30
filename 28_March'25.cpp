d8f68a1821ecb1e0571068fb85b5672b573c6f82
Kartik <gkhatri211@gmail.com>
2025-03-30 15:53:03 +0530

    numOfEnclaves

--- 28_March'25.cpp
+++ 28_March'25.cpp
@@ -51,3 +51,65 @@ public:
         return res;
     }
 };
+
+// 1020. Number of Enclaves
+// You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+// A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+// Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
+
+//APPROACH:-
+//BFS
+
+//CODE:-
+class Solution {
+    public:
+        int numEnclaves(vector<vector<int>>& grid) {
+            int tot=0;
+            int bound=0;
+            int m=grid.size();
+            int n=grid[0].size();
+            vector<vector<bool>> visited(m,vector<bool>(n,false));
+            queue<pair<int,int>> q;
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(i==0 || i==m-1 || j==n-1 || j==0){
+                        if(grid[i][j]==1){
+                            bound++;
+                            q.push({i,j});
+                            visited[i][j]=true;
+                        }
+                    }
+                    if(grid[i][j]==1){
+                        tot++;
+                    }
+                }
+            }
+            while(!q.empty()){
+                auto it=q.front();
+                q.pop();
+                int x=it.first;
+                int y=it.second;
+                if(x>0 && grid[x-1][y]==1 && !visited[x-1][y]){
+                    q.push({x-1,y});
+                    visited[x-1][y]=true;
+                    bound++;
+                }
+                if(x<m-1 && grid[x+1][y]==1 && !visited[x+1][y]){
+                    q.push({x+1,y});
+                    visited[x+1][y]=true;
+                    bound++;
+                }
+                if(y>0 && grid[x][y-1]==1 && !visited[x][y-1]){
+                    q.push({x,y-1});
+                    visited[x][y-1]=true;
+                    bound++;
+                }
+                if(y<n-1 && grid[x][y+1]==1 && !visited[x][y+1]){
+                    q.push({x,y+1});
+                    visited[x][y+1]=true;
+                    bound++;
+                }
+            }
+            return tot-bound;
+        }
+    };
