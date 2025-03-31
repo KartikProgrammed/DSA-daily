c61e5a41ea61720f39f7d8a28c8f234adee32ff9
Kartik <gkhatri211@gmail.com>
2025-03-31 16:36:12 +0530

    shortestPathBinMatrix

--- 31_March'25.cpp
+++ 31_March'25.cpp
@@ -137,3 +137,64 @@ public:
         return res;
     }
 };
+
+//1091. Shortest Path in Binary Matrix
+// Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+// A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+// All the visited cells of the path are 0.
+// All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
+// The length of a clear path is the number of visited cells of this path.
+
+//APPROACH:- DFS
+//TLE
+
+//CODE:-
+class Solution {
+public:
+    int dfs(vector<vector<int>>& grid,int x,int y,int dist,vector<vector<bool>> &visited){
+        int n=grid.size();
+        if(x==n-1 && y==n-1){
+            return dist;
+        }
+        visited[x][y]=true;
+        int res=INT_MAX;
+        if(x>0 && grid[x-1][y]==0 && !visited[x-1][y]){
+            res=min(res,dfs(grid,x-1,y,dist+1,visited));
+        }
+        if(x<n-1 && grid[x+1][y]==0 && !visited[x+1][y]){
+            res=min(res,dfs(grid,x+1,y,dist+1,visited));
+        }
+        if(y>0 && grid[x][y-1]==0 && !visited[x][y-1]){
+            res=min(res,dfs(grid,x,y-1,dist+1,visited));
+        }
+        if(y<n-1 && grid[x][y+1]==0 && !visited[x][y+1]){
+            res=min(res,dfs(grid,x,y+1,dist+1,visited));
+        }
+        if(x>0 && y>0 && grid[x-1][y-1]==0 && !visited[x-1][y-1]){
+            res=min(res,dfs(grid,x-1,y-1,dist+1,visited));
+        }
+        if(x<n-1 && y<n-1 && grid[x+1][y+1]==0 && !visited[x+1][y+1]){
+            res=min(res,dfs(grid,x+1,y+1,dist+1,visited));
+        }
+        if(x>0 && y<n-1 && grid[x-1][y+1]==0 && !visited[x-1][y+1]){
+            res=min(res,dfs(grid,x-1,y+1,dist+1,visited));
+        }
+        if(x<n-1 && y>0 && grid[x+1][y-1]==0 && !visited[x+1][y-1]){
+            res=min(res,dfs(grid,x+1,y-1,dist+1,visited));
+        }
+        visited[x][y] = false;
+        return res;
+    }
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<vector<bool>> visited(n,vector<bool>(n,false));
+        if(grid[0][0]==1){
+            return -1;
+        }
+        int res=dfs(grid,0,0,1,visited);
+        if(res==INT_MAX){
+            return -1;
+        }
+        return res;
+    }
+};
