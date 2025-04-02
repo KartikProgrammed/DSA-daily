2a1e37f6f55ace4c0ca61a6d3bc9ebeb0ddaa979
Kartik <gkhatri211@gmail.com>
2025-04-02 17:24:17 +0530

    bellmanford

--- 2_April'25.cpp
+++ 2_April'25.cpp
@@ -157,3 +157,41 @@ public:
         return res;
     }
 };
+
+
+//BELLMAN FORD
+//FIND NOTES IN CHEATSHEET
+
+//CODE:-
+class Solution {
+  public:
+    /*  Function to implement Bellman Ford
+     *   edges: vector of vectors which represents the graph
+     *   src: source vertex
+     *   V: number of vertices
+     */
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        vector<int> dist(V,1e8);
+        dist[src]=0;
+        for(int i=0;i<V-1;i++){
+            for(auto edge:edges){
+                int u=edge[0];
+                int v=edge[1];
+                int x=edge[2];
+                if(dist[u]!=1e8 && dist[u]+x<dist[v]){
+                    dist[v]=dist[u]+x;
+                }
+            }
+        }
+        for(auto edge:edges){
+                int u=edge[0];
+                int v=edge[1];
+                int x=edge[2];
+                if(dist[u]!=1e8 && dist[u]+x<dist[v]){
+                    return{-1};
+                }
+        }
+        
+        return dist;
+    }
+};
