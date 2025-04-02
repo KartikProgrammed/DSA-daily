90e43ed1a9585f1b90d2cff9d7c7b0de1b3f0a44
Kartik <gkhatri211@gmail.com>
2025-04-02 15:11:16 +0530

    CityWithSmallestNoOfNeigh

--- 2_April'25.cpp
+++ 2_April'25.cpp
@@ -94,3 +94,66 @@ public:
         return ways[n-1];
     }
 };
+
+// 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance
+// There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge between cities fromi and toi, and given the integer distanceThreshold.
+// Return the city with the smallest number of cities that are reachable through some path and whose distance is at most distanceThreshold, If there are multiple such cities, return the city with the greatest number.
+// Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' weights along that path.
+
+//APPROACH:-
+//dijkstra on all nodes separately
+
+//CODE:-
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<int> city(n,0);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<vector<pair<int,int>>> adj(n);
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            int x=edges[i][2];
+
+            adj[u].push_back({v,x});
+            adj[v].push_back({u,x});
+        }
+        for(int i=0;i<n;i++){
+            pq.push({0,i});
+            vector<int> dist(n,INT_MAX);
+            dist[i]=0;
+            while(!pq.empty()){
+                auto it=pq.top();
+                pq.pop();
+                int node=it.second;
+                int curr=it.first;
+                if(curr>distanceThreshold){
+                    continue;
+                }
+                
+                for(auto neigh:adj[node]){
+                    int tot=neigh.second+curr;
+                    int nnode=neigh.first;
+                    if(dist[nnode]>tot){
+                        dist[nnode]=tot;
+                        pq.push({tot,nnode});
+                    }
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                if (i != j && dist[j] <= distanceThreshold) {
+                    city[i]++;
+                }
+            }
+        }
+        int curr=INT_MAX;
+        int res=0;
+        for(int i=n-1;i>=0;i--){
+            if(city[i]<curr){
+                res=i;
+                curr=city[i];
+            }
+        }
+        return res;
+    }
+};
