5b1770cf9c62d08196c15ff72c683c0e52ed85af
Kartik <gkhatri211@gmail.com>
2025-04-02 14:39:22 +0530

    NoofWaysToReachDest

--- 2_April'25.cpp
+++ 2_April'25.cpp
@@ -40,3 +40,57 @@ public:
         return res;
     }
 };
+
+
+//1976. Number of Ways to Arrive at Destination
+// You are in a city that consists of n intersections numbered from 0 to n - 1 with bi-directional roads between some intersections. The inputs are generated such that you can reach any intersection from any other intersection and that there is at most one road between any two intersections.
+// You are given an integer n and a 2D integer array roads where roads[i] = [ui, vi, timei] means that there is a road between intersections ui and vi that takes timei minutes to travel. You want to know in how many ways you can travel from intersection 0 to intersection n - 1 in the shortest amount of time.
+// Return the number of ways you can arrive at your destination in the shortest amount of time. Since the answer may be large, return it modulo 109 + 7.
+
+//APPROACH:-
+//DIJKSTRA
+
+//CODE:-
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> pq;
+        vector<long long> dist(n,LLONG_MAX);
+        vector<int> ways(n,0);
+        ways[0]=1;
+        int MOD=1e9+7;
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto road:roads){
+            int u=road[0];
+            int v=road[1];
+            int x=road[2];
+            adj[u].push_back({v,x});
+            adj[v].push_back({u,x});
+        }
+        dist[0]=0;
+        pq.push({0,0});
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            long long time=it.first;
+            int node=it.second;
+            if(time>dist[node]){
+                continue;
+            }
+            for(auto neigh:adj[node]){
+                int nnode=neigh.first;
+                long long ntime=neigh.second;
+                long long tot=time+ntime;
+                if(ntime+time<dist[nnode]){
+                    dist[nnode]=tot;
+                    ways[nnode]=ways[node];
+                    pq.push({tot,nnode});
+                }
+                else if(tot==dist[nnode]){
+                    ways[nnode]=(ways[nnode]+ways[node]) % MOD;
+                }
+            }
+        }
+        return ways[n-1];
+    }
+};
