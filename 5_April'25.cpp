7e153fa35c84bfcbec624c2dc8b7c30248e5e1c5
Kartik <gkhatri211@gmail.com>
2025-04-05 21:46:57 +0530

    HouseRobber2

--- 5_April'25.cpp
+++ 5_April'25.cpp
@@ -147,3 +147,36 @@ public:
         return recursion(nums,0,0,true,memo);
     }
 };
+
+
+//213. House Robber II
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+// Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+//APPROACH:-
+//DP
+
+//CODE:-
+class Solution {
+public:
+    int recursion(vector<int>& nums, int curr, vector<int>& memo, int end) {
+        if (curr > end) return 0;
+        if (memo[curr] != -1) return memo[curr];
+
+        int rob = nums[curr] + recursion(nums, curr + 2, memo, end);
+        int skip = recursion(nums, curr + 1, memo, end);
+
+        return memo[curr] = max(rob, skip);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        vector<int> memo1(n, -1);
+        int case1 = recursion(nums, 0, memo1, n - 2);
+        vector<int> memo2(n, -1);
+        int case2 = recursion(nums, 1, memo2, n - 1);
+
+        return max(case1, case2);
+    }
+};
