f0553838dbb08db531e2eebcb955b93ff60b6b05
Kartik <gkhatri211@gmail.com>
2025-04-07 14:13:52 +0530

    UniquePaths2

--- 7_April'25.cpp
+++ 7_April'25.cpp
@@ -81,3 +81,45 @@ public:
     }
 };
 
+
+//63. Unique Paths II
+// You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+// An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+// Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+// The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+
+//APPROACH:-
+//DP
+
+//CODE:-
+class Solution {
+public:
+    int dp(int x,int y,int m,int n,vector<vector<int>> &memo,vector<vector<int>> &grid){
+        if(grid[x][y]==1){
+            return 0;
+        }
+        if(x==m-1 && y==n-1){
+            return 1;
+        }
+        if(memo[x][y]!=-1){
+            return memo[x][y];
+        }
+        int right=0,down=0;
+        if(x<m-1){
+            down+=dp(x+1,y,m,n,memo,grid);
+        }
+        if(y<n-1){
+            right+=dp(x,y+1,m,n,memo,grid);
+        }
+        return memo[x][y]=down+right;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>> memo(m,vector<int>(n,-1));
+
+        return dp(0,0,m,n,memo,obstacleGrid);
+
+    }
+};
