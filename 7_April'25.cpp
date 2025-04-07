73bed9a299db3b08aed94c3af7fd2e7cadfe55d5
Kartik <gkhatri211@gmail.com>
2025-04-07 14:06:17 +0530

    UniquePaths

--- 7_April'25.cpp
+++ 7_April'25.cpp
@@ -46,3 +46,37 @@ public:
         return dp(nums,sum/2,0,memo);
     }
 };
+
+
+//62. Unique Paths
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+//APPROACH:-
+//DP
+
+//CODE:-
+class Solution {
+public:
+    int dp(int x,int y,int m,int n,vector<vector<int>> &memo){
+        if(x==m-1 && y==n-1){
+            return 1;
+        }
+        if(memo[x][y]!=-1){
+            return memo[x][y];
+        }
+        int right=0,down=0;
+        if(x<m-1){
+            down+=dp(x+1,y,m,n,memo);
+        }
+        if(y<n-1){
+            right+=dp(x,y+1,m,n,memo);
+        }
+        return memo[x][y]=down+right;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> memo(m,vector<int>(n,-1));
+        return dp(0,0,m,n,memo);
+    }
+};
