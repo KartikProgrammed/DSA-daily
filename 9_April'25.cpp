2ea1731f295a49fc0d74256c0aa4844b605a9110
Kartik <gkhatri211@gmail.com>
2025-04-09 17:52:02 +0530

    MinOperaToMakeArrVal=k

--- 9_April'25.cpp
+++ 9_April'25.cpp
@@ -85,3 +85,40 @@ public:
         return res;
     }
 };
+
+
+//3375. Minimum Operations to Make Array Values Equal to K
+// You are given an integer array nums and an integer k.
+// An integer h is called valid if all values in the array that are strictly greater than h are identical.
+// For example, if nums = [10, 8, 10, 8], a valid integer is h = 9 because all nums[i] > 9 are equal to 10, but 5 is not a valid integer.
+// You are allowed to perform the following operation on nums:
+// Select an integer h that is valid for the current values in nums.
+// For each index i where nums[i] > h, set nums[i] to h.
+// Return the minimum number of operations required to make every element in nums equal to k. If it is impossible to make all elements equal to k, return -1.
+
+//APPROACH:-
+//just count the no. of int>k in the arr
+///problem phrased bad thats it
+
+//CODE:-
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int mini=*min_element(nums.begin(),nums.end());
+        if(mini<k){
+            return -1;
+        }
+        unordered_map<int,int> map;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]]++;
+            if(map[nums[i]]==1){
+                count++;
+            }
+        }
+        if(mini==k){
+            return count-1;
+        }
+        return count;
+    }
+};
