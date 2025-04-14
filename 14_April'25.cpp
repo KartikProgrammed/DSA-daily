3e4dee05f7b9dcf2f7d7a89991f456de33e71260
Kartik <gkhatri211@gmail.com>
2025-04-14 18:22:10 +0530

    CountGoodTriplets

---
+++ 14_April'25.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include<stack>
+#include <sstream>
+#include <map>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<numeric>
+#include<climits>
+using namespace std;
+
+// 1534. Count Good Triplets
+// Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+// A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+// 0 <= i < j < k < arr.length
+// |arr[i] - arr[j]| <= a
+// |arr[j] - arr[k]| <= b
+// |arr[i] - arr[k]| <= c
+// Where |x| denotes the absolute value of x.
+// Return the number of good triplets.
+
+//APROACH:-
+//run 3 loops and increase res if conditions satisfy
+
+//CODE:-
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int res=0;
+        for(int i=0;i<arr.size()-2;i++){
+            for(int j=i+1;j<arr.size()-1;j++){
+                for(int k=j+1;k<arr.size();k++){
+                    if(abs(arr[i]-arr[j]) <=a && abs(arr[j]-arr[k]) <=b && abs(arr[i]-arr[k]) <=c ){
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
