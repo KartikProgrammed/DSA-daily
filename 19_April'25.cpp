f7b21b076b6c22a7abd4419c044ed6e6f8e1b63b
Kartik <gkhatri211@gmail.com>
2025-04-19 14:01:54 +0530

    CountTheNo.OfFairPair

--- 19_April'25.cpp
+++ 19_April'25.cpp
@@ -43,3 +43,25 @@ public:
         return res;
     }
 };
+
+
+//Approach:-
+//Sorting approach
+//Sort the array and then use two pointers to find the number of fair pairs
+
+//CODE:-
+class Solution {
+public:
+    long long helper(vector<int>& nums,long long comp){
+        long long ans=0;
+        for(int i=0,j=nums.size()-1;i<j;i++){
+            while(i<j && nums[i]+nums[j]>comp) --j;
+            ans+=j-i;
+        }
+        return ans;
+    }
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(),nums.end());
+        return helper(nums,upper)-helper(nums,lower-1);
+    }
+};
