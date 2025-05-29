3993f37071a53d0f76be52ddb51ff011f13b8086
Kartik <gkhatri211@gmail.com>
2025-05-29 21:42:42 +0530

    LongestStringChain

--- 29_May'25.cpp
+++ 29_May'25.cpp
@@ -67,3 +67,60 @@ public:
         return res;
     }
 };  
+
+// 1048. Longest String Chain
+
+// You are given an array of words where each word consists of lowercase English letters.
+// wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without changing the order of the other characters to make it equal to wordB.
+// For example, "abc" is a predecessor of "abac", while "cba" is not a predecessor of "bcad".
+// A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1, where word1 is a predecessor of word2, word2 is a predecessor of word3, and so on. A single word is trivially a word chain with k == 1.
+// Return the length of the longest possible word chain with words chosen from the given list of words.
+
+// Approach:
+// 1. Sort the words by their lengths.
+//start with the longest word and try to find the longest chain by checking if the current word can be formed by removing one character from the previous word.
+
+//Code:-
+class Solution {
+public:
+    bool checkpossible(string &s1,string &s2){
+        if(s1.size()!=s2.size()+1){
+            return false;
+        }
+        int first=0;
+        int second=0;
+        while(first<s1.size()){
+            if(s1[first]==s2[second]){
+                first++;
+                second++;
+            }
+            else{
+                first++;
+            }
+        }
+        if(first==s1.size() && second==s2.size()){
+            return true;
+        }
+        return false;
+    }
+    static bool comp(string &s1,string &s2){
+        return s1.size()<s2.size();
+    }
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(), comp);
+        int n=words.size();
+        int maxi=1;
+        vector<int> dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(checkpossible(words[i],words[j]) && 1+dp[j]>dp[i]){
+                    dp[i]=1+dp[j];
+                }
+            }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+            }
+        }
+        return maxi;
+    }
+};
