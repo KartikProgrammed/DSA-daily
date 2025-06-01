1c0b780952c19d13f900693e27a7a83c9ea3c75d
Kartik <gkhatri211@gmail.com>
2025-06-01 18:51:54 +0530

    BurstBalloons

--- 1_June'25.cpp
+++ 1_June'25.cpp
@@ -144,3 +144,36 @@ public:
         return recursion(cuts,1,N,dp);
     }
 };
+
+// 312. Burst Balloons
+// You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
+// If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+// Return the maximum coins you can collect by bursting the balloons wisely.
+
+// Approach: Dynamic Programming with Memoization
+
+// CODE:-
+class Solution {
+public:
+    int recursion(vector<int> &nums,int st,int end,vector<vector<int>> &dp){
+        if(st>end){
+            return 0;
+        }
+        if(dp[st][end]!=-1){
+            return dp[st][end];
+        }
+        int res=INT_MIN;
+        for(int i=st;i<=end;i++){
+            int cost=nums[i]*nums[st-1]*nums[end+1]+recursion(nums,st,i-1,dp)+recursion(nums,i+1,end,dp);
+            res=max(res,cost);
+        }
+        return dp[st][end]=res;
+    }
+    int maxCoins(vector<int>& nums) {
+        int n=nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin(),1);
+        vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
+        return recursion(nums,1,n,dp);
+    }
+};
