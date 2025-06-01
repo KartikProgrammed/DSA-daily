861fd820de9f698bae7f179b2ad956c8daa316f2
Kartik <gkhatri211@gmail.com>
2025-06-01 18:32:25 +0530

    MinimumCostStickCut

--- 1_June'25.cpp
+++ 1_June'25.cpp
@@ -107,3 +107,40 @@ public:
 
     }
 };
+
+//1547. Minimum Cost to Cut a Stick
+// Given a wooden stick of length n units. The stick is labelled from 0 to n. For example, a stick of length 6 is labelled as follows:
+// Given an integer array cuts where cuts[i] denotes a position you should perform a cut at.
+// You should perform the cuts in order, you can change the order of the cuts as you wish.
+// The cost of one cut is the length of the stick to be cut, the total cost is the sum of costs of all cuts. When you cut a stick, it will be split into two smaller sticks (i.e. the sum of their lengths is the length of the stick before the cut). Please refer to the first example for a better explanation.
+// Return the minimum total cost of the cuts.
+
+// Approach: Dynamic Programming with Memoization
+//important line:- int cost=cuts[end+1]-cuts[st-1]+recursion(cuts,st,i-1,dp)+recursion(cuts,i+1,end,dp); dry run this line to understand how the cost is calculated.
+
+// CODE:-
+class Solution {
+public:
+    int recursion(vector<int> &cuts,int st,int end,vector<vector<int>> &dp){
+        if(st>end){
+            return 0;
+        }
+        if(dp[st][end]!=-1){
+            return dp[st][end];
+        }
+        int res=INT_MAX;
+        for(int i=st;i<=end;i++){
+            int cost=cuts[end+1]-cuts[st-1]+recursion(cuts,st,i-1,dp)+recursion(cuts,i+1,end,dp);
+            res=min(cost,res);
+        }
+        return dp[st][end]=res;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        int N=cuts.size();
+        sort(cuts.begin(),cuts.end());
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        vector<vector<int>> dp(N+1,vector<int>(N+1,-1));
+        return recursion(cuts,1,N,dp);
+    }
+};
