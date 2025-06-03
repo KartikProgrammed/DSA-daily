d36a45b99bcfe5767aa85bc4736b6e46b6b1c545
Kartik <gkhatri211@gmail.com>
2025-06-04 00:39:43 +0530

    ParsingBooleanExpression

--- 3_June'25.cpp
+++ 3_June'25.cpp
@@ -76,3 +76,63 @@ public:
         return res;
     }
 };
+
+
+//1106. Parsing A Boolean Expression
+// A boolean expression is an expression that evaluates to either true or false. It can be in one of the following shapes:
+// 't' that evaluates to true.
+// 'f' that evaluates to false.
+// '!(subExpr)' that evaluates to the logical NOT of the inner expression subExpr.
+// '&(subExpr1, subExpr2, ..., subExprn)' that evaluates to the logical AND of the inner expressions subExpr1, subExpr2, ..., subExprn where n >= 1.
+// '|(subExpr1, subExpr2, ..., subExprn)' that evaluates to the logical OR of the inner expressions subExpr1, subExpr2, ..., subExprn where n >= 1.
+// Given a string expression that represents a boolean expression, return the evaluation of that expression.
+// It is guaranteed that the given expression is valid and follows the given rules.
+
+
+// Approach: Stack
+
+// CODE:
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> st;
+        for(int i=0;i<expression.size();i++){
+            if(expression[i]==',' || expression[i]==' '){
+                continue;
+            }
+            if(expression[i]!=')'){
+                st.push(expression[i]);
+            }
+            else{
+                vector<bool> temp;
+                while(!st.empty() && (st.top() == 't' || st.top() == 'f')){
+                    temp.push_back(st.top()=='t');
+                    st.pop();
+                }
+                if (!st.empty() && st.top() == '(') st.pop();
+                char op=st.top();
+                st.pop();
+
+                bool result;
+
+                if(op=='!'){
+                    result=!temp[0];
+                }
+                else if(op=='|'){
+                    result=false;
+                    for(int j=0;j<temp.size();j++){
+                        result=result||temp[j];
+                    }
+                }
+                else{
+                    result=true;
+                    for(int j=0;j<temp.size();j++){
+                        result=result&&temp[j];        
+                    }
+                }
+                st.push(result?'t':'f');
+            }
+        }
+        return st.top()=='t';
+    }
+};
