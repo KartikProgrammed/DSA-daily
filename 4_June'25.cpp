301dae0ecb859145af56f3fc94f7864002da4871
Kartik <gkhatri211@gmail.com>
2025-06-04 23:04:55 +0530

    PalindromePartitioning

--- 4_June'25.cpp
+++ 4_June'25.cpp
@@ -44,3 +44,55 @@ public:
     }
 };
 
+
+//132. Palindrome Partitioning II
+// Given a string s, partition s such that every substring of the partition is a palindrome.
+// Return the minimum cuts needed for a palindrome partitioning of s.
+
+//Approach:
+//Use dynamic programming to find the minimum cuts needed for a palindrome partitioning of s.
+//at every index i, you can either cut at the index or not
+//if u do, then change the left pointer to i+1 and check if the string to the left was a palindrome or not
+//if it was, then you can add 1 to the cuts needed for the right side of the string
+
+// CODE:
+class Solution {
+public:
+    bool check(int left,int curr,string &s){
+        for(int i=left,j=curr;i<j;i++,j--){
+            if(s[i]!=s[j]){
+                return false;
+            }
+        }
+        return true;
+    }
+    int recursion(int left,int curr,string &s,vector<vector<int>> &dp){
+        if(curr==s.length()){
+            if(check(left,curr-1,s)){
+                return 0;
+            }
+            else{
+                return INT_MAX;
+            }
+        }
+        if(dp[left][curr]!=-1){
+            return dp[left][curr];
+        }
+        int res=INT_MAX;
+        int op1=INT_MAX,op2=INT_MIN;
+        if(check(left,curr,s)){
+            int next=recursion(curr+1,curr+1,s,dp);
+            if(next!=INT_MAX){
+                op1=1+next;
+            }
+        }
+        op2=recursion(left,curr+1,s,dp);
+        res=min(op1,op2);
+        return dp[left][curr]=res;
+    }
+    int minCut(string s) {
+        int n=s.length();
+        vector<vector<int>> dp(n+1,vector(n+1,-1));
+        return recursion(0,0,s,dp);
+    }
+};
