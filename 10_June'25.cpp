3f8c41760c494fe42a348751bb6b625e7c73c6fa
Kartik <gkhatri211@gmail.com>
2025-06-10 22:15:24 +0530

    Count&Say

--- 10_June'25.cpp
+++ 10_June'25.cpp
@@ -86,3 +86,45 @@ public:
         return count;
     }
 };
+
+//38. Count and Say
+// The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+// countAndSay(1) = "1"
+// countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+// Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed string becomes "23321511".
+// Given a positive integer n, return the nth element of the count-and-say sequence.
+
+//Approach:
+// Use a loop to generate the count-and-say sequence iteratively by counting consecutive characters in the previous string.
+
+// CODE:
+class Solution {
+public:
+    string rle(string n){
+        char prev=n[0];
+        int count=1;
+        string res="";
+        for(int i=1;i<n.length();i++){
+            if(prev!=n[i]){
+                res+=to_string(count)+prev;
+                count=1;
+                prev=n[i];
+            }
+            else{
+                count++;
+            }
+        }
+        res+=to_string(count)+prev;
+        return res;
+    }
+
+    string countAndSay(int n) {
+        string res="1";
+        int count=1;
+        while(count<n){
+            res=rle(res);
+            count++;
+        }
+        return res;
+    }
+};
