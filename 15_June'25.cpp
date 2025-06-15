5fcf9727da5c8fc0052ef2233f75dd021ce69e87
Kartik <gkhatri211@gmail.com>
2025-06-15 21:32:25 +0530

    MissingNum

--- 15_June'25.cpp
+++ 15_June'25.cpp
@@ -166,3 +166,26 @@ public:
         }
     }
 };
+
+//268. Missing Number
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Approach:
+//use a temporary array to keep track of the numbers present in the array.
+
+// CODE:
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        vector<bool> temp(nums.size()+1,false);
+        for(int i=0;i<nums.size();i++){
+            temp[nums[i]]=true;
+        }
+        for(int i=0;i<temp.size();i++){
+            if(!temp[i]){
+                return i;
+            }
+        }
+        return 0;
+    }
+};
