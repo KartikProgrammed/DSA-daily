c13c59d0bb0c670b3721e6a1f76ba7575d5b82c8
Kartik <gkhatri211@gmail.com>
2025-06-17 21:35:50 +0530

    TwoSum

--- 17_June'25.cpp
+++ 17_June'25.cpp
@@ -63,3 +63,31 @@ public:
         return result;
     }
 };
+
+//1. Two Sum
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+
+// Approach: Hash map to store indices of elements
+
+//CODE:-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int,int> map;
+        for(int i=0;i<nums.size();i++){
+            int nex=target-nums[i];
+            if(map.find(nex)!=map.end()){
+                res.push_back(i);
+                res.push_back(map[nex]);
+                return res;
+            }
+            map[nums[i]]=i;
+        }
+        return {};
+    }
+};
+ 
