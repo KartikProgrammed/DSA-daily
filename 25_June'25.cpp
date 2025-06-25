95c134920348a20d78a7245a320e00ea7fc7491d
Kartik <gkhatri211@gmail.com>
2025-06-25 23:43:49 +0530

    SubarrSum==K

--- 25_June'25.cpp
+++ 25_June'25.cpp
@@ -60,3 +60,29 @@ public:
         return res;
     }
 };
+
+
+//560. Subarray Sum Equals K
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Approach: Use a hash map to store the cumulative sum and its frequency. For each element, check if the cumulative sum minus k exists in the map. If it does, add the frequency of that sum to the result.
+
+//Code:-
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+        map[0]=1;
+        int count=0;
+        int tot=0;
+        for(int it:nums){
+            tot+=it;
+            if(map.find(tot-k)!=map.end()){
+                count+=map[tot-k];
+            }
+            map[tot]++;
+        }
+        return count;
+    }
+};
