8fb4e4f185ccd27c9e32df84626a790f06c1793a
Kartik <gkhatri211@gmail.com>
2025-06-26 22:09:49 +0530

    ThreeSum

--- 26_June'25.cpp
+++ 26_June'25.cpp
@@ -82,3 +82,50 @@ public:
         return length;
     }
 };
+
+
+//15. 3Sum
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
+
+
+// Approach: Sort the array and use a two-pointer technique to find pairs that sum to the negative of the current element.
+
+
+// CODE:
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>0){
+                    k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    vector<int> temp={nums[i],nums[j],nums[k]};
+                    res.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
+                    }
+                    while(j<k && nums[k]==nums[k+1]){
+                        k--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
