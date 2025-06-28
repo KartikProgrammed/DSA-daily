0011582d53ee1108524291e46426f8ea87d41cb8
Kartik <gkhatri211@gmail.com>
2025-06-28 16:49:43 +0530

    MergeSortedArr

--- 28_June'25.cpp
+++ 28_June'25.cpp
@@ -84,3 +84,43 @@ public:
         return res;
     }
 };
+
+//88. Merge Sorted Array
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// Approach:
+//use an extra array to store the merged result
+//copy the elements of nums1 and nums2 into the extra array
+//sort the extra array
+//copy the elements of the extra array back to nums1
+
+// CODE:
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       vector<int> nums;
+       int l=0;
+       int r=0;
+        while(l<m && r<n){
+            if(nums1[l]<nums2[r]){
+                nums.push_back(nums1[l]);
+                l++;
+            }
+            else{
+                nums.push_back(nums2[r]);
+                r++;
+            }
+        }
+        while(l<m){
+            nums.push_back(nums1[l++]);
+        }
+        while(r<n){
+            nums.push_back(nums2[r++]);
+        }
+        for(int i=0;i<nums1.size();i++){
+            nums1[i]=nums[i];
+        }
+    }
+};
