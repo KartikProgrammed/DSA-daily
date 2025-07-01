fb1475f7347ec76ffca62ab847ed2a34733f1d1c
Kartik <gkhatri211@gmail.com>
2025-07-01 22:30:50 +0530

    SearchInRotatedSortedArr

--- 1_July'25.cpp
+++ 1_July'25.cpp
@@ -50,3 +50,59 @@ public:
         return res+count;
     }
 };
+
+//33. Search in Rotated Sorted Array
+// There is an integer array nums sorted in ascending order (with distinct values).
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Approach:
+// 1. Use binary search to find the pivot index where the array is rotated.
+// 2. Determine which part of the array to search based on the target value.
+// 3. Perform a standard binary search in the identified part of the array.
+// 4. Return the index of the target if found, otherwise return -1.
+
+// CODE:
+class Solution {
+public:
+    int binsearch(vector<int>& nums,int target,int low,int high){
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>target){
+                high=mid-1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+    int findpiv(vector<int>& nums){
+        int low=0;
+        int high=nums.size()-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+    int search(vector<int>& nums, int target) {
+        int piv=findpiv(nums);
+        if(piv==0){
+            return binsearch(nums,target,0,nums.size()-1);
+        }
+        if(target>=nums[0]){
+            return binsearch(nums,target,0,piv-1);
+        }
+        return binsearch(nums,target,piv,nums.size()-1);      
+    }
+};
