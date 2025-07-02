4ce77875ba09b819972aa11adf2f7b618b827480
Kartik <gkhatri211@gmail.com>
2025-07-03 00:39:54 +0530

    peakElement

--- 2_July'25.cpp
+++ 2_July'25.cpp
@@ -97,3 +97,40 @@ public:
         return nums[low];
     }
 };
+
+//162. Find Peak Element
+// A peak element is an element that is strictly greater than its neighbors.
+// Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+// You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+// You must write an algorithm that runs in O(log n) time.
+
+// Approach:
+// 1. Initialize two pointers, left and right, to the start and end of the
+//    array.
+// 2. While left is less than right, calculate the middle index.
+// 3. If the middle element is greater than its right neighbor, it means
+//    the peak element is in the left half of the array, so move the right
+//    pointer to mid.
+// 4. If the middle element is less than its right neighbor, it means
+//    the peak element is in the right half of the array, so move the left
+//    pointer to mid + 1.
+// 5. If the middle element is equal to its right neighbor, it means
+//    the peak element is in the right half of the array, so move the left
+//    pointer to mid + 1.
+
+// CODE:
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+};
