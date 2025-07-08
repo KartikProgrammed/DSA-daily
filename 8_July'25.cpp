368ca86a8cb49a5acec8bc54351cb99144909886
Kartik <gkhatri211@gmail.com>
2025-07-09 03:02:51 +0530

    Search2DArrII

--- 8_July'25.cpp
+++ 8_July'25.cpp
@@ -149,3 +149,32 @@ public:
         return BSRow(matrix,target,col);
     }
 };
+
+//240. Search a 2D Matrix II
+// Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+// Integers in each row are sorted in ascending from left to right.
+// Integers in each column are sorted in ascending from top to bottom.
+
+// Approach:
+// Use binary search to find the target in the 2D matrix.
+
+// CODE:
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=0;
+        int col=matrix[0].size()-1;
+        while(row>=0 && row<matrix.size() && col>=0 && col<matrix[0].size()){
+            if(matrix[row][col]==target){
+                return true;
+            }
+            if(matrix[row][col]>target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+};
