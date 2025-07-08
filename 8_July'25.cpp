1b0e2157cf2b292b82e73ae39407dc817847ad35
Kartik <gkhatri211@gmail.com>
2025-07-09 03:20:16 +0530

    FindPeakElementIII

--- 8_July'25.cpp
+++ 8_July'25.cpp
@@ -178,3 +178,44 @@ public:
         return false;
     }
 };
+
+//1901. Find a Peak Element II
+// A peak element in a 2D grid is an element that is strictly greater than all of its adjacent neighbors to the left, right, top, and bottom.
+// Given a 0-indexed m x n matrix mat where no two adjacent cells are equal, find any peak element mat[i][j] and return the length 2 array [i,j].
+// You may assume that the entire matrix is surrounded by an outer perimeter with the value -1 in each cell.
+// You must write an algorithm that runs in O(m log(n)) or O(n log(m)) time.
+
+// Approach:
+// Use binary search to find a peak element in the 2D grid.
+
+// CODE:
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int low=0;
+        int high=mat[0].size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int maxRow=0;
+            for(int i=0;i<mat.size();i++){
+                if(mat[i][mid]>mat[maxRow][mid]){
+                    maxRow=i;
+                }
+            }
+
+            bool leftOk = (mid == 0) || (mat[maxRow][mid] > mat[maxRow][mid - 1]);
+            bool rightOk = (mid == mat[0].size() - 1) || (mat[maxRow][mid] > mat[maxRow][mid + 1]);
+
+            if (leftOk && rightOk) {
+                return {maxRow, mid};
+            }
+            else if (mid > 0 && mat[maxRow][mid - 1] > mat[maxRow][mid]) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return {};
+    }
+};
