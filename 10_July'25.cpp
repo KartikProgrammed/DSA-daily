886424b86b5530c8431f12c9cb4570506f9796a0
Kartik <gkhatri211@gmail.com>
2025-07-10 23:38:46 +0530

    ReverseWordsInString

--- 10_July'25.cpp
+++ 10_July'25.cpp
@@ -54,3 +54,43 @@ public:
         return res;
     }
 };
+
+//151. Reverse Words in a String
+// Given an input string s, reverse the order of the words.
+// A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+// Return a string of the words in reverse order concatenated by a single space.
+// Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+// Approach:
+//put all the words in a vector and then reverse the vector and join the words with a single space
+
+// CODE:
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> temp;
+        string curr="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' '){
+                if(!curr.empty()){
+                    temp.push_back(curr);
+                    curr="";
+                }
+            }
+            else{
+                curr+=s[i];
+            }
+        }
+        string res="";
+        if(!curr.empty()){
+            temp.push_back(curr);
+        }
+        for(int i=temp.size()-1;i>=0;i--){
+            if(i!=temp.size()-1){
+                res+=" ";
+            }
+            res+=temp[i];
+        }
+        return res;
+    }
+}; 
