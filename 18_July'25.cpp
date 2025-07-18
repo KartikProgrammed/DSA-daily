ce7479aa4fe332f9960b0335fc61e2b4d5033c82
Kartik <gkhatri211@gmail.com>
2025-07-18 23:08:34 +0530

    OddEvenLL

--- 18_July'25.cpp
+++ 18_July'25.cpp
@@ -151,3 +151,48 @@ public:
         return palindrome(vec);
     }
 };
+
+
+//328. Odd Even Linked List
+// Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+// The first node is considered odd, and the second node is even, and so on.
+// Note that the relative order inside both the even and odd groups should remain as it was in the input.
+// You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+// Approach:
+// 1. Use two pointers, one for odd indexed nodes and one for even indexed nodes.
+// 2. Traverse the linked list and link odd indexed nodes together and even indexed nodes together.
+// 3. Connect the last odd indexed node to the first even indexed node.
+// 4. Set the next of the last even indexed node to NULL.
+
+// CODE:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* temp1=head;
+        ListNode* temp2=head->next;
+        ListNode* head2=head->next;
+        while(temp2!=NULL && temp2->next!=NULL){
+            temp1->next=temp2->next;
+            temp1=temp1->next;
+            temp2->next=temp1->next;
+            temp2=temp2->next;
+        }
+        temp1->next=head2;
+        return head;
+
+    }
+};
