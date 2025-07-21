89840557868567eef17d5860de8015bce695e66b
Kartik <gkhatri211@gmail.com>
2025-07-21 20:48:30 +0530

    ReverseNodesinKgroups

--- 21_July'25.cpp
+++ 21_July'25.cpp
@@ -58,3 +58,74 @@ public:
         return res;
     }
 };
+
+//25. Reverse Nodes in k-Group
+// Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+// k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+// You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+// Approach:
+//No extra memory is used.
+
+//CODE:-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* first=NULL;
+        ListNode* second=head;
+        while(second!=NULL){
+            ListNode* third=second->next;
+            second->next=first;
+            first=second;
+            second=third;
+        }
+        return first;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* curr=head;
+        ListNode* prev=&dummy;
+        while(true){
+            ListNode* temp=curr;
+            int count=0;
+            while(temp!=NULL && count<k){
+                count++;
+                temp=temp->next;
+            }
+            if(count<k){
+                break;
+            }
+
+            ListNode* nextGroup = temp;
+            ListNode* groupStart = curr;
+            ListNode* groupEnd = curr;
+            for (int i = 1; i < k; ++i)
+                groupEnd = groupEnd->next;
+
+            groupEnd->next=NULL;
+
+            ListNode* newst=reverse(groupStart);
+            prev->next=newst;
+            groupStart->next=nextGroup;
+            
+            curr=nextGroup;
+            prev=groupStart;
+
+        }
+        return dummy.next;
+    }
+};
