520c72830207b21eff910ace6c6b1076af71151f
Kartik <gkhatri211@gmail.com>
2025-07-27 22:22:47 +0530

    CombinationSum

--- 27_July'25.cpp
+++ 27_July'25.cpp
@@ -47,3 +47,36 @@ public:
         return res;
     }
 };
+
+//39. Combination Sum
+// Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+// The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+// The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+// Approach:
+// Use backtracking to explore all combinations of candidates that sum to the target.
+
+// CODE:
+class Solution {
+public:
+    void backtrack(vector<vector<int>> &res, vector<int> &curr, int ind, vector<int> &candidates, int target) {
+        if (target == 0) {
+            res.push_back(curr);
+            return;
+        }
+        if (target < 0 || ind == candidates.size()) return;
+
+        backtrack(res, curr, ind + 1, candidates, target);
+
+        curr.push_back(candidates[ind]);
+        backtrack(res, curr, ind, candidates, target - candidates[ind]);
+        curr.pop_back();
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> curr;
+        backtrack(res, curr, 0, candidates, target);
+        return res;
+    }
+};
