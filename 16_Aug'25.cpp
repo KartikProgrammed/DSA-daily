06bfefc549cc46e48e7b6a8028b0ad746ccebf13
Kartik <gkhatri211@gmail.com>
2025-08-16 21:48:29 +0530

    OnlineStockSpan

--- 16_Aug'25.cpp
+++ 16_Aug'25.cpp
@@ -73,3 +73,52 @@ public:
         return res;
     }
 };
+
+
+//901. Online Stock Span
+
+// Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+// The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward) for which the stock price was less than or equal to the price of that day.
+// For example, if the prices of the stock in the last four days is [7,2,1,2] and the price of the stock today is 2, then the span of today is 4 because starting from today, the price of the stock was less than or equal 2 for 4 consecutive days.
+// Also, if the prices of the stock in the last four days is [7,34,1,2] and the price of the stock today is 8, then the span of today is 3 because starting from today, the price of the stock was less than or equal 8 for 3 consecutive days.
+// Implement the StockSpanner class:
+// StockSpanner() Initializes the object of the class.
+// int next(int price) Returns the span of the stock's price given that today's price is price.
+
+
+// Approach:
+// 1. Use a stack to keep track of the prices and their spans.
+// 2. For each price, pop elements from the stack until the top of the stack has a price greater than the current price.
+// 3. The span for the current price is the difference between the current index and the
+// index of the last price that was greater than the current price.
+// 4. Push the current price and its span onto the stack.
+// 5. Return the span for the current price.
+
+// Code:
+class StockSpanner {
+private:
+    stack<pair<int,int>> stk;
+public:
+    StockSpanner() {
+    }
+    
+    int next(int price) {
+        if(stk.empty() || stk.top().first > price){
+            stk.push({price,1});
+            return 1;
+        }
+        int temp=1;
+        while(!stk.empty() && stk.top().first<=price){
+            temp+=stk.top().second;
+            stk.pop();
+        }
+        stk.push({price,temp});
+        return temp;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
