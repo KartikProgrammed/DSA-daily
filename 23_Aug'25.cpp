7f533499b36c38a40a5211b3b443f364bc4dce28
Kartik <gkhatri211@gmail.com>
2025-08-23 21:22:14 +0530

    MinAreaToCoverAll1II

---
+++ 23_Aug'25.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <limits>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+
+// 3197. Find the Minimum Area to Cover All Ones II
+
+// You are given a 2D binary array grid. You need to find 3 non-overlapping rectangles having non-zero areas with horizontal and vertical sides such that all the 1's in grid lie inside these rectangles.
+// Return the minimum possible sum of the area of these rectangles.
+// Note that the rectangles are allowed to touch.
+
+//CODE:-
+class Solution {
+public:
+
+    vector<vector<int>> rotateClockWise(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> rotatedGrid(n, vector<int>(m));
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                rotatedGrid[j][m-i-1] = grid[i][j];
+            }
+        }
+
+        return rotatedGrid;
+    }
+
+    int minimumArea(int startRow, int endRow, int startCol, int endCol, vector<vector<int>>& grid) {
+        int m = grid.size(); //Rows
+        int n = grid[0].size(); //Columns
+
+        int minRow = m;
+        int maxRow = -1;
+        int minCol = n;
+        int maxCol = -1;
+
+        for(int i = startRow; i < endRow; i++) {
+            for(int j = startCol; j < endCol; j++) {
+                if(grid[i][j] == 1) {
+                    minRow = min(minRow, i);
+                    maxRow = max(maxRow, i);
+
+                    minCol = min(minCol, j);
+                    maxCol = max(maxCol, j);
+                }       
+            }
+        }
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+    }
+
+    //O(m^2 * n^2)
+    int utility(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int result = INT_MAX;
+
+        for(int rowSplit = 1; rowSplit < m; rowSplit++) {
+            for(int colSplit = 1; colSplit < n; colSplit++) {
+
+                int top = minimumArea(0, rowSplit, 0, n, grid);
+                int bottomLeft = minimumArea(rowSplit, m, 0, colSplit, grid);
+                int bottomRight = minimumArea(rowSplit, m, colSplit, n, grid);
+
+                result = min(result, top + bottomLeft + bottomRight);
+
+
+                int topLeft = minimumArea(0, rowSplit, 0, colSplit, grid);
+                int topRight = minimumArea(0, rowSplit, colSplit, n, grid);
+                int bottom = minimumArea(rowSplit, m, 0, n, grid);
+
+                result = min(result, topLeft + topRight + bottom);
+
+            }
+        }
+
+        for(int split1 = 1; split1 < m; split1++) {
+            for(int split2 = split1+1; split2 < m; split2++) {
+                int top = minimumArea(0, split1, 0, n, grid);
+                int middle = minimumArea(split1, split2, 0, n, grid);
+                int bottom = minimumArea(split2, m, 0, n, grid);
+
+                result = min(result, top + middle + bottom);
+            }
+        }
+        return result;
+    }
+
+    int minimumSum(vector<vector<int>>& grid) {
+        
+        int result = utility(grid);
+
+        vector<vector<int>> rotatedGrid = rotateClockWise(grid);
+
+        result = min(result, utility(rotatedGrid));
+
+        return result;
+
+    }
+};
